701153b8bba9618f1dadb6b3d1dafb3ac0878f44
krikliviy <egorkrikliviy@gmail.com>
2025-04-23 11:52:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import factorial
+from utils import is_prime
+
+if __name__ == "__main__":
+	print(f"Факторіал числа {n}:", factorial(n))
+
+if __name__ == "__main__":
+	print(f"Чи є число {n} проcтим? - {is_prime(n)}")
